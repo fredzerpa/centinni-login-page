e9099ec950e48ef3753580796b5e6c285c4c4d2e
TOR <52118178+fredzerpa@users.noreply.github.com>
2020-07-19 17:28:13 -0400

    Updates

---
+++ precache-manifest.cb6b67bda8ef0427bd5f7223e6dda81e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e1918ba79cff9893dcc152b562c08ee",
+    "url": "/centinni-login-page/index.html"
+  },
+  {
+    "revision": "29af3126b93417f2544f",
+    "url": "/centinni-login-page/static/css/main.97e87560.chunk.css"
+  },
+  {
+    "revision": "273565d1fe3a0c1b06a7",
+    "url": "/centinni-login-page/static/js/2.832467e4.chunk.js"
+  },
+  {
+    "revision": "531d7b2065717e21d8dbdd1394bf24a4",
+    "url": "/centinni-login-page/static/js/2.832467e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29af3126b93417f2544f",
+    "url": "/centinni-login-page/static/js/main.28c7c630.chunk.js"
+  },
+  {
+    "revision": "80cada28ff495895deb6",
+    "url": "/centinni-login-page/static/js/runtime-main.5f967b4c.js"
+  },
+  {
+    "revision": "8354e2640b1c02c738c3e7e67e372359",
+    "url": "/centinni-login-page/static/media/facebook.8354e264.svg"
+  },
+  {
+    "revision": "09aea0f59807f6f4f66af7f5719cba9e",
+    "url": "/centinni-login-page/static/media/google.09aea0f5.svg"
+  },
+  {
+    "revision": "c9f1200836515fd4bacd29160be78732",
+    "url": "/centinni-login-page/static/media/twitter.c9f12008.svg"
+  }
+]);
